403e486a247fb6dfa88c44840ec7997039b37658
shikisummer <630930890@qq.com>
2018-05-17 15:53:01 +0800

    WebView test 1.1更行后退监听

--- app/src/main/java/com/example/yiweishi/runoob/WebViewActivity.java
+++ app/src/main/java/com/example/yiweishi/runoob/WebViewActivity.java
@@ -2,6 +2,8 @@ package com.example.yiweishi.runoob;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -14,7 +16,7 @@ public class WebViewActivity extends AppCompatActivity {
         setContentView(R.layout.activity_web_view);
 
         //webView实现
-        WebView mWebView = (WebView)findViewById(R.id.message_webView);
+        final WebView mWebView = (WebView)findViewById(R.id.message_webView);
         mWebView.loadUrl("http://www.sina.com.cn");
         //允许使用JS
         mWebView.getSettings().setJavaScriptEnabled(true);
@@ -23,10 +25,23 @@ public class WebViewActivity extends AppCompatActivity {
          * * LOAD_NO_CACHE: 不使用缓存，只从网络获取数据.
          * * LOAD_CACHE_ELSE_NETWORK，只要本地有，无论是否过期，或者no-cache，都使用缓存中的数据。 */
         //不使用缓存只从网路获取
-        mWebView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
+        //mWebView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
 
         //使页面在本页面显示而非系统浏览器
         mWebView.setWebViewClient(new WebViewClient());
+        //后退监听
+        mWebView.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (event.getAction()==KeyEvent.ACTION_DOWN){
+                    if(keyCode==KeyEvent.KEYCODE_BACK && mWebView.canGoBack()){
+                        mWebView.goBack();
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
     }
 
 }
